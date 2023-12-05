cd2dd4c22a86a8adb274e88a5f1e8e4fcbfef75b
J-Cheol <lunaris0206@naver.com>
2023-12-05 16:30:05 +0900

    2-05 : answer check

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -20,19 +20,26 @@ class SbbMissionApplicationTests
 
     @Autowired
     private AnswerRepository answerRepository;
+
     @Test
     void testJpa()
     {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q); // 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요하다.
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(2, a.getQuestion().getId());
     }
+//    { // answer data create after save
+//        Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        Answer a = new Answer();
+//        a.setContent("네 자동으로 생성됩니다.");
+//        a.setQuestion(q); // 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요하다.
+//        a.setCreateDate(LocalDateTime.now());
+//        this.answerRepository.save(a);
+//    }
 
 //    { //delete
 //        assertEquals(2, this.questionRepository.count());
@@ -94,7 +101,6 @@ class SbbMissionApplicationTests
 //        q2.setCreateDate(LocalDateTime.now());
 //        this.questionRepository.save(q2); // 두번째 질문 저장
 //    }
-
 
 
 }
