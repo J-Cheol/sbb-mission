452c2a9363d1ff7668287cf9e46887d3313f7000
J-Cheol <lunaris0206@naver.com>
2023-12-05 16:22:42 +0900

    2-05 : delete

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -19,13 +19,22 @@ class SbbMissionApplicationTests
     private QuestionRepository questionRepository;
     @Test
     void testJpa()
-    { //modify
+    {
+        assertEquals(2, this.questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
+
+//    { //modify
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        q.setSubject("수정된 제목");
+//        this.questionRepository.save(q);
+//    }
 
 //    { // findBySubjectLike
 //        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
